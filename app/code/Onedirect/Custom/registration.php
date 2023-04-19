fb81d09d39051f622f570c351f9810d10ea0bb46
Adria Plazuelo <aplazuelob73@cepnet.net>
2023-04-19 13:43:10 +0200

    Ultimo commit prueba terminada

--- app/code/Onedirect/Custom/registration.php
+++ app/code/Onedirect/Custom/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Learning_FirstUnit',
+    'Onedirect_Custom',
     __DIR__
 );
